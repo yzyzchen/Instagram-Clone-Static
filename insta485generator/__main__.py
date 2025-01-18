c159b3bf8e172efa54772c0c01ff171ba30fd038
Yuzhou Chen <yzc@umich.edu>
2025-01-18 16:57:52 -0500

    generator setuo

---
+++ insta485generator/__main__.py
@@ -0,0 +1,17 @@
+"""Build static HTML site from directory of HTML templates and plain files."""
+import flask
+
+# @click.command()
+# @click.argument("input_dir", nargs=1, type=click.Path(exists=True))
+# def main(input_dir):
+#     input_dir = pathlib.Path(input_dir)
+#     print(f"DEBUG input_dir={input_dir}")
+
+
+def main():
+    """Top level command line interface."""
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
